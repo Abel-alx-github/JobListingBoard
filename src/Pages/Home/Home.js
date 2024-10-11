ceca61ef034452cdc0178d7510af9320b11c7538
Abel Tibeso <abeltibeso323@gmail.com>
2024-10-11 22:31:12 +0300

    jobs Category card elemnt is add to home

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,7 +1,4 @@
-import React, {useState} from 'react'
-import styles from './Home.module.scss'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSearch } from '@fortawesome/free-solid-svg-icons'
+
 import bannerGif from "../../Assets/bannerGif.gif";
 import item0 from '../../Assets/sliderItem0.png'
 import item1 from '../../Assets/slideritem1.png'
@@ -11,65 +8,38 @@ import item4 from '../../Assets/slideritem4.png'
 import item5 from '../../Assets/slideritem5.png'
 import item6 from '../../Assets/slideritem6.png'
 
+import Slider from '../../Components/Slider/Slider'
+import Hero from '../../Components/Hero/Hero'
+import styles from './Home.module.scss'
+import CategoryCard from "../../Components/CategoryCard/CategoryCard";
+const sliderImages = [
+  item0, item1, item2, item3, item4, item5, item6, item0, item1, item2, item3, item4, item5, item6,
+  item0, item1, item2, item3, item4, item5, item6, item0, item1, item2, item3, item4, item5, item6,
+  item0, item1, item2, item3, item4, item5, item6, item0, item1, item2, item3, item4, item5, item6
+]
+const categoryItems = [ item2, item3, item4, item5, item6, item0, item1, item2, item3];
 export default function Home() {
-  const [isJob, setIsJob] = useState(true);
  
   return (
     <div className='container'>
-       <div className={styles.heroContainer}>
-       <div className={`${styles.row}  `} >
-          <div className={styles.col2}>
-            <h1 className={styles.heroTitle}>Remote Jobs</h1>
-            <p className={styles.heroTagline}>Unlock Your Career Potential</p>
-            <a href="#" className={styles.heroBtn}>Get Started</a>
-            <div className={styles.heroSearch}>
-              <div className={styles.heroSearchToggle}>
-                <a className={`${isJob? styles.active: ''}`} href="#" onClick={()=> setIsJob(true)}>Jobs</a>
-                <a className={`${!isJob? styles.active: ''}`} href="#" onClick={()=> setIsJob(false)}>Companies</a>
-              </div>
-              <div className={styles.heroSearchContainer}>
-                <i><FontAwesomeIcon icon={faSearch} /></i>
-                <input type="text" placeholder={`${"Search for"}  ${isJob ? "jobs" : "companies"}`} />
-                <button type="search">Search</button>
-              </div>
-            </div>
-          </div>
-          <div className={`${styles.col2} ${styles.heroImg}`}>
-            <div className={styles.img}></div> 
-          </div>
-
-        </div>
-        </div>  
+      <Hero />     
         <div className={styles.bannerGif}>
           <img src={bannerGif} alt="Expo banner 2024" />
         </div>
-        <div className={styles.sliderContainer}>
-          <div className={styles.slider}>
-          <img  className={styles.slideItem} src={item0} alt="" />
-          <img  className={styles.slideItem} src={item1} alt="" />
-          <img  className={styles.slideItem} src={item2} alt="" />
-          <img  className={styles.slideItem} src={item3} alt="" />
-          <img  className={styles.slideItem} src={item4} alt="" />
-          <img  className={styles.slideItem} src={item5} alt="" />
-          <img  className={styles.slideItem} src={item6} alt="" />
-          <img  className={styles.slideItem} src={item0} alt="" />
-          <img  className={styles.slideItem} src={item1} alt="" />
-          <img  className={styles.slideItem} src={item2} alt="" />
-          <img  className={styles.slideItem} src={item3} alt="" />
-          <img  className={styles.slideItem} src={item4} alt="" />
-          <img  className={styles.slideItem} src={item5} alt="" />
-          <img  className={styles.slideItem} src={item6} alt="" />
-        
-          <img  className={styles.slideItem} src={item0} alt="" />
-          <img  className={styles.slideItem} src={item1} alt="" />
-          <img  className={styles.slideItem} src={item2} alt="" />
-          <img  className={styles.slideItem} src={item3} alt="" />
-          <img  className={styles.slideItem} src={item4} alt="" />
-          <img  className={styles.slideItem} src={item5} alt="" />
-          <img  className={styles.slideItem} src={item6} alt="" />
-        
-          </div>
-        </div>
+  
+        <h2 className={styles.sliderHeadingTitle}>Trusted By</h2>
+        <Slider images={sliderImages} />
+        <h2 className={styles.categoriesHeadingTitle}>Top Categories</h2>
+       
+       <div className={styles.categoryContainer}>
+         {
+          categoryItems.map((image, idx) => {
+            return <CategoryCard key={idx} image={image} title={`Category ${idx}`} />
+            })
+        }
+            <CategoryCard className={styles.categoryItem} image={item3} title={`Category 333`} />
+
+       </div>
       </div>
   )
-}
+} 
